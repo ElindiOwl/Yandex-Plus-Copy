4b57a17a4fb671a8dce64246211d2407bcb5c6e4
ElindiOwl <prostonaf@gmail.com>
2024-02-27 04:01:17 +0300

    feat(Main): Added the initial card with no events and adaptive design

--- src/Components/Main/MainComponents/FirstCard/FirstCard.tsx
+++ src/Components/Main/MainComponents/FirstCard/FirstCard.tsx
@@ -1,13 +1,30 @@
 import './FirstCard.scss';
+import React, { useState } from 'react';
+
 
 const FirstCard = () => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 1 ? 0 : prevIndex + 1));
+    };
+
+    const prevSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? 1 : prevIndex - 1));
+    };
+
+    const transformStyle = {
+        transform: `translateX(-${currentIndex * 7}%)`,
+    };
+
     return (
         <section className='firstCardSection'>
             <div className='card-text'>
                 <h2 className='firstH'>Спортивный сезон в Плюсе</h2>
                 <p className='firstP'>Болейте за любимые команды</p>
             </div>
-            <div className='CardSet'>
+            <div className='CardSet' style={transformStyle}>
                 <button className='frCard'>
                     <div className='frCardInside'>
                         <h2>Билеты <br /> на плеф-офф Кубка Гагарина</h2>
@@ -29,6 +46,24 @@ const FirstCard = () => {
                     </div>
                 </button>
             </div>
+            {currentIndex === 1 && (
+                <button className='prevButton' onClick={prevSlide}>
+                    <svg viewBox='1 3 21 21' fill='none' xmlns='http://www.w3.org/2000/svg' width='20' height='20'>
+                        <path fillRule='evenodd' clipRule='evenodd'
+                              d='M15.566 4.434a.8.8 0 0 1 0 1.13L9.13 12l6.435 6.434a.8.8 0 1 1-1.132 1.132l-7-7a.8.8 0 0 1 0-1.131l7-7a.8.8 0 0 1 1.132 0Z'
+                              fill='#21201F' />
+                    </svg>
+                </button>
+            )}
+            {currentIndex === 0 && (
+                <button className='nextButton' onClick={nextSlide}>
+                    <svg viewBox='1 3 21 21' fill='none' xmlns='http://www.w3.org/2000/svg' width='20' height='20'>
+                        <path fillRule='evenodd' clipRule='evenodd'
+                              d='M8.434 4.434a.8.8 0 0 0 0 1.13L14.87 12l-6.435 6.434a.8.8 0 0 0 1.132 1.132l7-7a.8.8 0 0 0 0-1.131l-7-7a.8.8 0 0 0-1.132 0Z'
+                              fill='#21201F' />
+                    </svg>
+                </button>
+            )}
         </section>
     );
 };
