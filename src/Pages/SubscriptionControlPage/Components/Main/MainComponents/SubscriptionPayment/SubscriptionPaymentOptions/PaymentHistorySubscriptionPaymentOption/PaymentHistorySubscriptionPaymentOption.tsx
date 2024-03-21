6a2927067d224eb68b4f4223026a44138ffd22d0
ElindiOwl <prostonaf@gmail.com>
2024-03-21 18:30:55 +0300

    feat(Subscription):

--- src/Pages/SubscriptionControlPage/Components/Main/MainComponents/SubscriptionPayment/SubscriptionPaymentOptions/PaymentHistorySubscriptionPaymentOption/PaymentHistorySubscriptionPaymentOption.tsx
+++ src/Pages/SubscriptionControlPage/Components/Main/MainComponents/SubscriptionPayment/SubscriptionPaymentOptions/PaymentHistorySubscriptionPaymentOption/PaymentHistorySubscriptionPaymentOption.tsx
@@ -1,12 +1,12 @@
-import './PaymentHistorySubPaymentOption.scss';
-import PaymentsHistoryMap from './SubControlHistoryPage/PaymentHistoryMap/PaymentsHistoryMap';
+import './PaymentHistorySubscriptionPaymentOption.scss';
+import PaymentsHistoryMap from './SubscriptionControlHistoryPage/PaymentHistoryMap/PaymentsHistoryMap';
 
 const PaymentHistoryPaymentOption = () => {
 
     return (
         <div className='paymentHistorySubPayment'>
             <h2>История списаний</h2>
-            <PaymentsHistoryMap displayCount={3} />
+            <PaymentsHistoryMap displayCount={3} showDateHolder={false} />
             <a href='/subControlHistoryPage'>Посмотреть списания за полгода</a>
         </div>
     );
