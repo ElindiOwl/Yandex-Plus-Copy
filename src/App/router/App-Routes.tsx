af524c6bd1044808321166fb3646bb0b33ee07af
ElindiOwl <prostonaf@gmail.com>
2024-06-19 15:29:57 +0300

    feat(main): testing

--- src/App/router/App-Routes.tsx
+++ src/App/router/App-Routes.tsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from 'react-router'
-import HomePage from 'src/Pages/homePage/Home-Page'
-import SubscriptionControlPage from 'src/Pages/Subscription-Control-Page/Subscription-Control-Page.tsx'
+import { Route, Routes } from 'react-router';
+import HomePage from 'src/Pages/homePage/Home-Page';
+import SubscriptionControlPage from 'src/Pages/Subscription-Control-Page/Subscription-Control-Page.tsx';
 import PaymentsHistoryPage
-	from 'src/Pages/Payments-History-Page/Payments-History-Page.tsx'
-import NotFoundPage from 'src/Pages/Not-Found-Page/Not-Found-Page'
+	from 'src/Pages/Payments-History-Page/Payments-History-Page.tsx';
+import NotFoundPage from 'src/Pages/Not-Found-Page/Not-Found-Page';
 
 export const AppRoutes = () => {
 	return (
 		<Routes>
-			<Route element={<HomePage />} path='/' />
+			<Route element={<HomePage />} path='/Yandex-Plus-Copy' />
 			<Route element={<SubscriptionControlPage />} path='/subControl' />
 			<Route element={<PaymentsHistoryPage />} path='/subControlHistoryPage' />
 			<Route element={<NotFoundPage />} path='*' />
 		</Routes>
-	)
-}
+	);
+};
