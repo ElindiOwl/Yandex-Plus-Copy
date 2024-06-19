e7f4ffc33abdf03ce2951866eadca2aa8f7847a3
ElindiOwl <prostonaf@gmail.com>
2024-06-19 15:03:40 +0300

    feat(main): fixed base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'https://elindiowl.github.io/Yandex-Plus-Copy',
+	base: './',
 	plugins: [react()],
 	resolve: {
 		alias: {
