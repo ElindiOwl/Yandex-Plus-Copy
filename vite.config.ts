a586802da0aeeae1b6e7e19725b78ed255a7dbd7
ElindiOwl <prostonaf@gmail.com>
2024-06-19 17:34:26 +0300

    feat(main): testing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './Yandex-Plus-Copy/',
+	base: 'https://elindiowl.github.io/Yandex-Plus-Copy/',
 	plugins: [react()],
 	resolve: {
 		alias: {
